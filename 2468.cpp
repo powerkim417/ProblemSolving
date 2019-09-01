26e348d05cb8e12f7468647f40c732543d0d025c
Mardwe <powerkim417@naver.com>
2019-09-02 03:20:01 +0900

    190901 문제 조금
    그냥 문제

---
+++ 2468.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int map[100][100];
+bool visited[100][100];
+
+void dfs(int i, int j){
+
+}
+
+int main(void){
+    cin>>n;
+    for (int i=0 ; i<n ; i++){
+        for (int j=0 ; j<n ; j++){
+            cin>>map[i][j];
+        }
+    }
+    int max = 0;
+    for (int rain=1 ; rain<=100 ; rain++){
+
+
+
+
+        if (temp == 0) break;
+    }
+}
